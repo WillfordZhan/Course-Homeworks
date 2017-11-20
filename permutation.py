35714e7004062fa5a09b3ab0d5b863cd3322a533
WillfordZhan <willfordzhan@gmail.com>
2017-11-20 18:56:27 +0800

    finished permutation

--- permutation.py
+++ permutation.py
@@ -1,18 +1,26 @@
 def permutation_r(n):
-	head = n[len()-1];
-	n.pop(-1);
-	if (n.len()<=1):
-		sequence = [];
-		sequence.append(n[0]);
-		return sequence;
-	else
-		sequence = permutation_r(n);
-		baseStr = sequence[0];
-		baseStrNum = sequence.len();
-		for i in sequence:
-			sequence[i] = head + sequence[i];
-		for i in xrange(0,baseStr):
-			elementToSwap = baseStr[i];
-			for j in xrange(0,baseStrNum):
-				sequence.append(sequence[j].replace(head,elementToSwap));
-			
+	head = n[len(n)-1]
+	if (len(n)<=1):
+		sequence1 = []
+		sequence1.append(n[0])
+		return sequence1
+	else:
+		sequence = permutation_r(n[:-1])
+		baseStr = sequence[0]
+		baseStrNum = len(sequence)
+		i = 0
+		while i < len(sequence):
+			sequence[i] = head + sequence[i]
+			i += 1
+		for k in baseStr:
+			elementToSwa = k
+			for j in range(0,baseStrNum):
+				ele = sequence[j]
+				eleSwapInd = ele.index(elementToSwa)
+				ele = ele.replace(head,elementToSwa)
+				ele = ele[:eleSwapInd] + head + ele[eleSwapInd + 1:]
+				sequence.append(ele)
+		return sequence
+
+a = ["A", "B", "C"]
+print(permutation_r(a))
